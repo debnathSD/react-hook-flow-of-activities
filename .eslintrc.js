ed4a163e33be6c63a285434b8e62c9dbb7304115
Sourav Debnath <sourav2012d@gmail.com>
2021-08-22 18:03:02 +0530

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: [
-      'plugin:react/recommended',
+      "plugin:react/recommended",
       'plugin:react-hooks/recommended',
       'plugin:prettier/recommended',
     ],
